133e24a84b4242a886f36821844edee27c5a6ddf
Anwesh43 <anweshthecool0@gmail.com>
2019-05-27 10:47:31 +0530

    adding TPCNode class

--- tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
+++ tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
@@ -125,8 +125,52 @@ class TriPlusCreateView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class TPCNode(var i : Int, val state : State = State()) {
+
+        private var next : TPCNode? = null
+        private var prev : TPCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TPCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTPCNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TPCNode {
+            var curr : TPCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
