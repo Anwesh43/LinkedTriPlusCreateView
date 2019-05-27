f2efc05980a1db50332bd679c4a2bc50357d0efc
Anwesh43 <anweshthecool0@gmail.com>
2019-05-27 10:38:31 +0530

    adding TriPlusCreateView class

--- tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
+++ tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
@@ -64,3 +64,21 @@ fun Canvas.drawTPCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TriPlusCreateView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
