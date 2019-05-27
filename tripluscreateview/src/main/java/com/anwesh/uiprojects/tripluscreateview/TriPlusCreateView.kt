155ac827f985d5663cf0e7235af56d8fc079949b
Anwesh43 <anweshthecool0@gmail.com>
2019-05-27 10:03:45 +0530

    adding constants and libraries

--- tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
+++ tripluscreateview/src/main/java/com/anwesh/uiprojects/tripluscreateview/TriPlusCreateView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.tripluscreateview
 /**
  * Created by anweshmishra on 27/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
